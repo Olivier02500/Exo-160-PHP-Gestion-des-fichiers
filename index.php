0ceb1d12bc9487ce74814e0754696bc0f6bb2d06
Olivier02500 <olivier.peuchet@hotmail.com>
2022-01-25 09:30:46 +0100

    make exo 160

--- index.php
+++ index.php
@@ -7,6 +7,12 @@
  */
 // TODO Votre code ici.
 $lorem = " Lorem ipsum dolor sit amet, consectetur adipisicing elit. ";
+$files = file_put_contents('texte.txt' , '$lorem');
+if (file_exists('texte.txt')) {
+    readfile('texte.txt');
+}
+
+echo "<br>";
 
 /**
  * 3. Créez un tableau contenant au moins 10 chaînes de caractère au choix.
@@ -16,19 +22,38 @@ $lorem = " Lorem ipsum dolor sit amet, consectetur adipisicing elit. ";
  */
 // TODO Votre code ici
 
+$table = ['Lorem',
+    ' ipsum',
+    'dolor',
+    'sit',
+    'amet',
+    'consectetur',
+    'adipisicing',
+    'elit',
+    'sit',
+    'amet'];
+if (file_exists('texte.txt')) {
+    file_put_contents('texte.txt' ,print_r($table,true));
+}
 
 /**
  * 5. Trouvez une solution pour afficher à l'aide d'un simple echo l'extension du fichier index.php
  */
 // TODO Votre code ici
 
-
+echo dirname('index.php') . "<br>";
 
 /**
  * 6. Testez si le fichier 'toto' existe, sil n'existe pas, afficher un texte distant que ce fichier n'existe pas !
  */
 // TODO Votre code ici.
 
+if (!file_exists(toto.txt)){
+    echo "<p> le fichier toto n'existe pas </p>";
+}
+else {
+    echo "<p> le fichier toto existe  </p>";
+}
 
 /**
  * Super bonus.
